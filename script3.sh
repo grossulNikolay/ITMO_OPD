54b643426c7e1a8894035caacc05d32b54c5fb53
ISEEDEADPEOPLE <nickolas.ross@icloud.com>
2026-09-20 13:25:48 +0300

    Create script3.sh

---
+++ script3.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+cd ~/lab0
+
+chmod 777 hitmontop
+chmod 777 purugly
+chmod 777 combusken
+chmod 777 combusken/ralts
+chmod 777 purugly/gothorita
+chmod 777 hitmontop/swoobat
+chmod 777 foongus
+
+ln -s hitmontop Copy_50
+
+ln mudkip purugly/krabbymudkip
+
+cp -r hitmontop combusken/ralts/
+
+cp tangela hitmontop/lediantangela
+
+cp foongus purugly/gothorita/
+
+ln -s ~/lab0/mudkip combusken/poliwagmudkip
+
+cat hitmontop/boldore purugly/swellow > foongus_85
+
+chmod 511 hitmontop
+chmod 750 purugly
+chmod 357 combusken
+chmod 570 combusken/ralts
+chmod 570 purugly/gothorita
+chmod 357 hitmontop/swoobat
+chmod 006 foongus
