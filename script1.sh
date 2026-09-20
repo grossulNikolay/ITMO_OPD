f73288d01982536f0f59d343e2a50626fc149a90
ISEEDEADPEOPLE <nickolas.ross@icloud.com>
2026-09-20 13:23:19 +0300

    Create script1.sh
    Creating tree

---
+++ script1.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+mkdir -m 777 lab0
+ln -s /home/studs/s561415/OPD/Lab1/lab0 ~/lab0
+cd ~/lab0
+
+mkdir combusken
+mkdir combusken/ralts
+
+echo "Способности = Water Sport Bubble
+Hypnosis Water Gun Doubleslap Rain Dance
+Body Slam Bubblebeam Mud Shot Belly Drum
+Wake-up Slap Hydro Pump Mud Bomb" > combusken/poliwag
+
+mkdir combusken/shellder
+
+echo "weight=277.3 height=47.0 atk=9 def=14" > combusken/forretress
+
+echo "Тип диеты = Phototroph" > foongus
+
+mkdir hitmontop
+mkdir hitmontop/meganium
+
+echo "Способности = Swarm Mach Speed Early
+Bird Frisk" > hitmontop/ledian
+
+mkdir hitmontop/swoobat
+
+echo "Способности =This is dummy text when
+pokemon does not contain something. It
+is better than NPE!"  > hitmontop/ludicolo
+
+mkdir hitmontop/mamoswine
+
+echo "weight=224.9 height=35.0 atk=11 def=11"  > hitmontop/boldore
+
+echo "Способности = Torrent Oblivious Damp" > mudkip
+
+mkdir purugly
+mkdir purugly/gothorita
+
+echo "Живет = Forest Grassland"  > purugly/swellow
+
+mkdir purugly/silcoon
+
+echo "Способности = Bubble Mud Sport Vicegrip
+Leer Harden Bubblebeam Mud Shot Metal
+Claw Stomp Protect Guillotine Slam Brine
+Crabhammer Flail" > purugly/krabby
+
+echo "Возможности = Overland=6 Surface=2
+Jump=2 Power=2 Intelligence=4 Reach=0"  > tangela
