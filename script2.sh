ce2e5d4c05c5f3855fe850b0d6f9efe53a7610db
ISEEDEADPEOPLE <nickolas.ross@icloud.com>
2026-09-20 13:24:45 +0300

    Create script2.sh
    Giving rights

---
+++ script2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+cd ~/lab0
+
+chmod u=rx,g=rwx,o= combusken/ralts
+chmod 666 combusken/poliwag
+chmod u=rx,g=rx,o=x combusken/shellder
+chmod u=rw,g=rw,o=r combusken/forretress
+chmod 006 foongus
+chmod u=rwx,g=rx,o= hitmontop/meganium
+chmod u=rw,g=rw,o= hitmontop/ledian
+chmod u=wx,g=rx,o=rwx hitmontop/swoobat
+chmod u=r,g=r,o= hitmontop/ludicolo
+chmod u=rx,g=rwx,o=x hitmontop/mamoswine
+chmod u=rw,g=r,o=r hitmontop/boldore
+chmod 404 mudkip
+chmod u=rx,g=rwx,o= purugly/gothorita
+chmod u=rw,g=rw,o=r purugly/swellow
+chmod u=wx,g=x,o=rx purugly/silcoon
+chmod u=,g=rw,o=rw purugly/krabby
+chmod 600 tangela
+
+chmod 357 combusken
+chmod 511 hitmontop
+chmod 750 purugly
