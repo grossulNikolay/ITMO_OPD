f9b1ff993879d811bbe81f5e0d73a1fce30c11d8
ISEEDEADPEOPLE <nickolas.ross@icloud.com>
2026-09-20 13:26:17 +0300

    Create script4.sh

---
+++ script4.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd ~/lab0
+
+cat l* */l* */*/l* 2>&1 | wc -m | sort -n
+
+ls -lRt 2>/dev/null | grep "fo"
+
+cat hitmontop/* 2>/tmp/errors3.txt | grep -vi "ke"
+
+cat *e */*e */*/*e 2>/dev/null | wc -m > /tmp/result4.txt
+
+ls -lR 2>/tmp/errors5.txt | grep "di" | sort -nk5 | tail -n 4
+
+ls -lRrt combusken 2>/dev/null
