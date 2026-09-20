0226f2f06b89443e1a5cac7d2a8378a6b0c37464
ISEEDEADPEOPLE <nickolas.ross@icloud.com>
2026-09-20 13:26:51 +0300

    Create script5.sh

---
+++ script5.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cd ~/lab0
+
+chmod 777 combusken
+chmod 777 combusken/ralts
+chmod 777 purugly
+
+rm -f foongus
+
+rm -rf combusken/forretress
+
+rm -f Copy_*
+
+rm -f purugly/krabbymudk*
+
+rm -rf combusken/ralts
+
+rm -rf combusken
